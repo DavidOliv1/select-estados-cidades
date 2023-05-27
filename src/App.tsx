ae957ed5a0d7a871d05f25bd68b5e12166bb2803
David Oliveira <davidstna21@gmail.com>
2023-05-27 11:42:47 -0300

    Adicionando evento de change no select de estados e criando state selectEstado, garantindo select dinâmico das cidades com base no estado selecionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import './App.css';
 import useEstados from './hooks/useEstados';
 import useCidades from './hooks/useCidades';
+import { useState } from 'react';
 
 function App() {
   const { estados } = useEstados();
-  const { cidades } = useCidades({ uf: 'se' });
+  const [selectEstado, setSelectEstado] = useState('');
+  const { cidades } = useCidades({ uf: selectEstado });
+
+  const handleUpdateEstado = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectEstado(e.target.value);
+  };
+
   return (
     <div>
-      <select>
+      <select onChange={handleUpdateEstado}>
         {estados.map((estado) => (
-          <option key={estado.id}>{estado.nome}</option>
+          <option key={estado.id} value={estado.sigla}>
+            {estado.nome}
+          </option>
         ))}
       </select>
       <select>
