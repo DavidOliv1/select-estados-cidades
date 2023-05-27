0a75188fd92e22c74baf3e2917891fc708ead212
David Oliveira <davidstna21@gmail.com>
2023-05-27 11:33:07 -0300

    Consumindo hook useCidades e montando select das cidades de forma estática

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css';
 import useEstados from './hooks/useEstados';
+import useCidades from './hooks/useCidades';
 
 function App() {
   const { estados } = useEstados();
+  const { cidades } = useCidades({ uf: 'se' });
   return (
     <div>
       <select>
         {estados.map((estado) => (
           <option key={estado.id}>{estado.nome}</option>
+        ))}
+      </select>
+      <select>
+        {cidades.map((cidade) => (
+          <option key={cidade.id}>{cidade.nome}</option>
         ))}
       </select>
     </div>
